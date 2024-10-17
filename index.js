479f476c7cc6320e48dcd001617c83bd0992d280
davidgarciamt <david.garcia@softwaresolutionssouthwest.com>
2024-10-17 13:10:33 -0700

    Added commit

--- index.js
+++ index.js
@@ -1,33 +1,15 @@
-function sayHello(name) {
-  console.log('Hello, ' + name);
-}
-sayHello('world');
-const arr = [1, 2, 3];
-arr.forEach((item) => {
-  console.log(item);
-});
-let x = 5;
-let y = 10;
-let z = x + y;
-console.log(z);
-function calculateSum(a, b) {
-  return a + b;
-}
-let sum = calculateSum(4, 5);
-for (let i = 0; i < 10; i++) {
-  console.log('Number:', i);
-}
-const obj = { name: 'John', age: 30 };
-console.log(obj.name, obj.age);
-if (x > y) {
-  console.log('x is greater.');
-} else {
-  console.log('y is greater');
-}
+function sayHello(name) {console.log('Hello, ' + name); }
+sayHello('world' );
+const arr=[1,2,3];arr.forEach((item)=> {console.log(item);});
+let x=5; let y = 10; let z = x + y; console.log(z);
+function calculateSum(a,b){ return a + b;} let sum=calculateSum(4 , 5);
+for(let i=0;i<10;i++){console.log('Number:',i);}
+const obj={name:'John',age:30};console.log(obj.name,obj.age);
+if(x>y){console.log('x is greater.');}else{console.log('y is greater');}
 
-const variable1 = 1;
-const variable2 = 1;
-const variable3 = 1;
-const variable4 = 1;
-const variable5 = 1;
-const variable6 = 1;
+const variable1 = 1
+const variable2 = 1 ;
+const variable3   = 1
+const variable4=1
+const variable5= 1
+const variable6= 1                          
