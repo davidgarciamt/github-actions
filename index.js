1fcbddbff1e5cfbbb2472ffd30d292eef2bbc6a6
davidgarciamt <david.garcia@softwaresolutionssouthwest.com>
2024-10-17 14:18:24 -0700

    Add commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const arr=[1,2,3];arr.forEach((item)=> {console.log(item);});
 let x=5; let y = 10; let z = x + y; console.log(z);
 function calculateSum(a,b){ return a + b;} let sum=calculateSum(4 , 5);
 for(let i=0;i<10;i++){console.log('Number:',i);}
-const obj={name:'John',age:30};console.log(obj.name,obj.age);
+const obj={name:'John',age:31};console.log(obj.name,obj.age);
 if(x>y){console.log('x is greater.');}else{console.log('y is greater');}
 
 const variable1 = 1
